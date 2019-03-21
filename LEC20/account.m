5ac533341de909c68f9e7f141115fd3947b3555e
Kamal Ranaweera <ranaweer@ualberta.ca>
2019-03-21 15:32:06 -0600

    Simple user-defined class

---
+++ LEC20/account.m
@@ -0,0 +1,25 @@
+classdef account
+   properties
+       Owner
+       Balance
+   end
+   methods
+       function obj = account(ownerName)
+           obj.Owner = ownerName;
+           obj.Balance = 0;
+       end
+       
+      function printBalance(obj)
+          fprintf('Balance: %.2f\n', obj.Balance)
+      end
+      function obj = deposit(obj, amount)
+          obj.Balance = obj.Balance + amount;
+      end
+      function obj = withdraw(obj, amount)
+          if obj.Balance < amount
+              error('Insufficient Balance');
+          end
+          obj.Balance = obj.Balance - amount;
+      end      
+   end
+end
